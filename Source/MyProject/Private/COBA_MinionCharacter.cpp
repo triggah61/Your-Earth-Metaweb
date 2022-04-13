29820b4058675e838cd4e959fffd2157d6159ad2
HAL9000\wmsau <william.sauve@decenternet.net>
2022-04-13 15:41:52 +0900

    Keep working on sequencer.
    Test out C++ class for COBA minion.

---
+++ Source/MyProject/Private/COBA_MinionCharacter.cpp
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "COBA_MinionCharacter.h"
+
+// Sets default values
+ACOBA_MinionCharacter::ACOBA_MinionCharacter()
+{
+ 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACOBA_MinionCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ACOBA_MinionCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+// Called to bind functionality to input
+void ACOBA_MinionCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
